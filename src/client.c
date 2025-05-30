d664250d427793223a776a877b9b3275fa88ebd0
Joe Fang (via Code Server) <25814618+MinecraftFuns@users.noreply.github.com>
2025-05-30 16:19:25 -0400

    feat: enhance control and experimental connection handling in client and server
    run-ci

--- src/client.c
+++ src/client.c
@@ -49,15 +49,17 @@ uint64_t time_us()
 
 int main(int argc, char *argv[])
 {
-    char *addr = NULL;
-    int port = 0;
+    char *ctrl_addr = NULL;
+    int ctrl_port = 0;
+    int exp_port = 0;
     int size = 0;
     int count = 0;
     char *output = NULL;
 
     static struct option long_options[] = {
         {"addr", required_argument, 0, 'a'},
-        {"port", required_argument, 0, 'p'},
+        {"control-port", required_argument, 0, 'P'},
+        {"exp-port", required_argument, 0, 'e'},
         {"size", required_argument, 0, 's'},
         {"count", required_argument, 0, 'c'},
         {"output", required_argument, 0, 'o'},
@@ -65,15 +67,18 @@ int main(int argc, char *argv[])
 
     int opt;
     int option_index = 0;
-    while ((opt = getopt_long(argc, argv, "a:p:s:c:o:", long_options, &option_index)) != -1)
+    while ((opt = getopt_long(argc, argv, "a:P:e:s:c:o:", long_options, &option_index)) != -1)
     {
         switch (opt)
         {
         case 'a':
-            addr = optarg;
+            ctrl_addr = optarg;
             break;
-        case 'p':
-            port = atoi(optarg);
+        case 'P':
+            ctrl_port = atoi(optarg);
+            break;
+        case 'e':
+            exp_port = atoi(optarg);
             break;
         case 's':
             size = atoi(optarg);
@@ -85,36 +90,70 @@ int main(int argc, char *argv[])
             output = optarg;
             break;
         default:
-            fprintf(stderr, "Usage: %s -a <address> -p <port> -s <bytes> -c <number> -o <file>\n", argv[0]);
+            fprintf(stderr, "Usage: %s -a <address> -P <control_port> [-e <exp_port>] -s <bytes> -c <number> -o <file>\n", argv[0]);
             return EXIT_FAILURE;
         }
     }
 
-    if (!addr || port <= 0 || size <= 0 || count <= 0 || !output)
+    if (!ctrl_addr || ctrl_port <= 0 || size <= 0 || count <= 0 || !output)
     {
-        fprintf(stderr, "Usage: %s -a <address> -p <port> -s <bytes> -c <number> -o <file>\n", argv[0]);
+        fprintf(stderr, "Usage: %s -a <address> -P <control_port> [-e <exp_port>] -s <bytes> -c <number> -o <file>\n", argv[0]);
         return EXIT_FAILURE;
     }
+    if (exp_port <= 0)
+        exp_port = ctrl_port + 1;
 
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
+    // Negotiate on control channel
+    int ctrl_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (ctrl_fd < 0)
     {
-        perror("socket");
+        perror("socket control");
         return EXIT_FAILURE;
     }
 
     struct sockaddr_in serv;
     memset(&serv, 0, sizeof(serv));
     serv.sin_family = AF_INET;
-    serv.sin_port = htons(port);
-    if (inet_pton(AF_INET, addr, &serv.sin_addr) <= 0)
+    serv.sin_port = htons(ctrl_port);
+    if (inet_pton(AF_INET, ctrl_addr, &serv.sin_addr) <= 0)
     {
-        perror("inet_pton");
+        perror("inet_pton control");
         return EXIT_FAILURE;
     }
+    if (connect(ctrl_fd, (struct sockaddr *)&serv, sizeof(serv)) < 0)
+    {
+        perror("connect control");
+        return EXIT_FAILURE;
+    }
+
+    struct
+    {
+        uint32_t size;
+        uint32_t count;
+        uint16_t exp_port;
+    } neg_net;
+    neg_net.size = htonl(size);
+    neg_net.count = htonl(count);
+    neg_net.exp_port = htons(exp_port);
+    if (send_all(ctrl_fd, &neg_net, sizeof(neg_net)) < 0)
+    {
+        perror("send negotiation");
+        return EXIT_FAILURE;
+    }
+    close(ctrl_fd);
+
+    // Experimental connection
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        perror("socket experiment");
+        return EXIT_FAILURE;
+    }
+
+    serv.sin_port = htons(exp_port);
     if (connect(sockfd, (struct sockaddr *)&serv, sizeof(serv)) < 0)
     {
-        perror("connect");
+        perror("connect experiment");
         return EXIT_FAILURE;
     }
 
--- src/server.c
+++ src/server.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <stdint.h>
 
 #define BACKLOG 1
 #define BUFSIZE 65536
@@ -23,63 +24,129 @@ int send_all(int sockfd, const void *buf, size_t len)
     return 0;
 }
 
+// recv_all ensures all data is received
+int recv_all(int sockfd, void *buf, size_t len)
+{
+    size_t total = 0;
+    char *p = buf;
+    while (total < len)
+    {
+        ssize_t n = recv(sockfd, p + total, len - total, 0);
+        if (n <= 0)
+            return -1;
+        total += n;
+    }
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     if (argc != 3 || strcmp(argv[1], "--port") != 0)
     {
-        fprintf(stderr, "Usage: %s --port <port>\n", argv[0]);
+        fprintf(stderr, "Usage: %s --port <control_port>\n", argv[0]);
         return EXIT_FAILURE;
     }
-    int port = atoi(argv[2]);
+    int control_port = atoi(argv[2]);
 
-    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (listen_fd < 0)
+    // Control listener setup
+    int control_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (control_fd < 0)
     {
         perror("socket");
         return EXIT_FAILURE;
     }
 
     int opt = 1;
-    setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    setsockopt(control_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
     struct sockaddr_in addr;
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = htons(port);
+    addr.sin_port = htons(control_port);
 
-    if (bind(listen_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    if (bind(control_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
     {
         perror("bind");
         return EXIT_FAILURE;
     }
-    if (listen(listen_fd, BACKLOG) < 0)
+    if (listen(control_fd, BACKLOG) < 0)
     {
         perror("listen");
         return EXIT_FAILURE;
     }
+    printf("Control listening on port %d...\n", control_port);
 
-    printf("Listening on port %d...\n", port);
-    int conn_fd = accept(listen_fd, NULL, NULL);
+    // Negotiation phase
+    int conn_fd = accept(control_fd, NULL, NULL);
     if (conn_fd < 0)
     {
         perror("accept");
         return EXIT_FAILURE;
     }
-    printf("Client connected\n");
+    printf("Client connected for negotiation\n");
+
+    struct
+    {
+        uint32_t size;
+        uint32_t count;
+        uint16_t exp_port;
+    } neg_net;
+    if (recv_all(conn_fd, &neg_net, sizeof(neg_net)) < 0)
+    {
+        perror("recv negotiation");
+        return EXIT_FAILURE;
+    }
+    uint32_t size = ntohl(neg_net.size);
+    uint32_t count = ntohl(neg_net.count);
+    uint16_t exp_port = ntohs(neg_net.exp_port);
+
+    close(conn_fd);
+    close(control_fd);
+
+    // Experimentation listener setup
+    int exp_listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (exp_listen_fd < 0)
+    {
+        perror("socket");
+        return EXIT_FAILURE;
+    }
+    setsockopt(exp_listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    addr.sin_port = htons(exp_port);
+    if (bind(exp_listen_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("bind");
+        return EXIT_FAILURE;
+    }
+    if (listen(exp_listen_fd, BACKLOG) < 0)
+    {
+        perror("listen");
+        return EXIT_FAILURE;
+    }
+    printf("Experiment listening on port %u...\n", exp_port);
+
+    int exp_fd = accept(exp_listen_fd, NULL, NULL);
+    if (exp_fd < 0)
+    {
+        perror("accept");
+        return EXIT_FAILURE;
+    }
+    printf("Experiment connection established\n");
 
     char buf[BUFSIZE];
-    ssize_t n;
-    while ((n = recv(conn_fd, buf, BUFSIZE, 0)) > 0)
+    for (uint32_t i = 0; i < count; i++)
     {
-        if (send_all(conn_fd, buf, n) < 0)
+        ssize_t n = recv(exp_fd, buf, BUFSIZE, 0);
+        if (n <= 0)
+            break;
+        if (send_all(exp_fd, buf, n) < 0)
         {
             perror("send");
             break;
         }
     }
 
-    close(conn_fd);
-    close(listen_fd);
+    close(exp_fd);
+    close(exp_listen_fd);
     return EXIT_SUCCESS;
 }
